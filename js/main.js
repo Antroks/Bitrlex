8a00f9769e6b7764c81f2390b9e5eed21fda159d
Antroks <tolik.popov.94@mail.ru>
2024-10-08 11:08:40 +1100

    add menu 767 with js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function openModal() {
+    document.getElementById("modal").style.left = "0px";
+    document.getElementById("sidenav-overlay").style.display = "block";
+    
+    
+}
+
+function closeModal() {
+    document.getElementById("modal").style.left = "-429px";
+    document.getElementById("sidenav-overlay").style.display = "none";
+}
+
+// $(document).on('click','.modal',function(e){
+//     $('.sidenav-overlay').css({'display': 'block'})
+        
+// })
+
+// $(document).on('click','.modal',function(e){
+//     $('.sidenav-overlay').css({'display': 'none'})
+    
+// })
+
